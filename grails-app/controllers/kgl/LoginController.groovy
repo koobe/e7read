3e342dbf3f48806f47167da698f1d5ed60c6a69c
lyhcode <lyhcode@gmail.com>
2014-07-21 18:35:09 +0800

    Add custom login form copy from springsecurity plugin

--- grails-app/controllers/kgl/LoginController.groovy
+++ grails-app/controllers/kgl/LoginController.groovy
@@ -1,10 +1,36 @@
 package kgl
 
 import grails.converters.JSON
+import grails.plugin.springsecurity.SpringSecurityUtils
 
 class LoginController {
 
+    /**
+     * Dependency injection for the authenticationTrustResolver.
+     */
+    def authenticationTrustResolver
+
+    /**
+     * Dependency injection for the springSecurityService.
+     */
+    def springSecurityService
+
     def index() {}
+
+    // Custom Auth Page
+    def auth() {
+        def config = SpringSecurityUtils.securityConfig
+
+        if (springSecurityService.isLoggedIn()) {
+            redirect uri: config.successHandler.defaultTargetUrl
+            return
+        }
+
+        String view = 'auth'
+        String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
+        render view: view, model: [postUrl: postUrl,
+                                   rememberMeParameter: config.rememberMe.parameter]
+    }
 
     def oauth() {
 

29a0d436b0df713e87c84ff74318eee44839e5f3
maxcloude <maxcloude@gmail.com>
2014-07-23 21:19:38 +0800

    Open iframe by bootstrap responsive feature.

--- grails-app/assets/javascripts/content_show.js
+++ grails-app/assets/javascripts/content_show.js
@@ -17,21 +17,29 @@ $(function() {
 
 function receiveMessage(event) {
 	console.log('Receive message: ' + event.data);
-	$('#iframe-show-content').remove();
+	
+	$('.div-iframe').remove();
+	$('.div-iframe').css('display', 'none');
 	$('body').css('overflow', '');
 	
 	onshowiframe = false;
 }
 
-var iframe = '<iframe id="iframe-show-content" frameborder="0" style="display:none;overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:fixed;top:0px;left:0px;right:0px;bottom:0px;z-index:999;" height="100%" width="100%"></iframe>';
+var responsiveiframe = '<div class="div-iframe embed-responsive" style="display:none; height:100%; width:100%; position:fixed;top:0px;left:0px;"><iframe class="iframe embed-responsive-item" src=""></iframe></div>';
+//var iframe = '<iframe id="iframe-show-content" frameborder="0" style="display:none;overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:fixed;top:0px;left:0px;right:0px;bottom:0px;z-index:999;" height="100%" width="100%"></iframe>';
 
 function showContent(contentId) {
-	
 	console.log('Open content: ' + contentId);
-	onshowiframe = true;
 	
-	$('body').append(iframe);
+	onshowiframe = true;
+//	
+//	$('body').append(iframe);
+//	$('body').css('overflow', 'hidden');
+//	$('#iframe-show-content').css('display', '');
+//	$('#iframe-show-content').attr('src', '/content/show/' + contentId);
+	
+	$('body').append(responsiveiframe);
 	$('body').css('overflow', 'hidden');
-	$('#iframe-show-content').css('display', '');
-	$('#iframe-show-content').attr('src', '/content/show/' + contentId);
+	$('.div-iframe').css('display', '');
+	$('.iframe').attr('src', '/content/show/' + contentId);
 }

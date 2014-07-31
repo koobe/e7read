8862f6595d6ef80c335464df4d68fcbf8593594c
lyhcode <lyhcode@gmail.com>
2014-07-31 12:29:03 +0800

    remove kendoui

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -74,7 +74,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.4"
+        compile ":asset-pipeline:1.9.6"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.15"

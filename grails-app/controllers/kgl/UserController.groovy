059c38e95b16f2e2f0e588d576a659f71d30f58a
cloudelin <maxcloude@gmail.com>
2014-08-01 17:48:39 +0800

    Remove read-only in transactional annotation on user controller.

--- grails-app/controllers/kgl/UserController.groovy
+++ grails-app/controllers/kgl/UserController.groovy
@@ -6,7 +6,7 @@ import grails.plugin.springsecurity.annotation.Secured
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
-@Transactional(readOnly = true)
+@Transactional
 class UserController {
 
     def springSecurityService

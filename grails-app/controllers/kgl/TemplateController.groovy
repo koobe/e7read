3c1450b98cdee64d4b5594164fb1fa8a71f1f088
cloudelin <maxcloude@gmail.com>
2014-08-04 16:30:12 +0800

    fix the mysql read only issue for template controller.

--- grails-app/controllers/kgl/TemplateController.groovy
+++ grails-app/controllers/kgl/TemplateController.groovy
@@ -8,7 +8,7 @@ import grails.plugin.springsecurity.annotation.Secured
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
-@Transactional(readOnly = true)
+@Transactional
 class TemplateController {
 	
 	GroovyPagesTemplateEngine groovyPagesTemplateEngine

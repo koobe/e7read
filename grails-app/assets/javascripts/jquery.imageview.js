bf956e5adde70418a184418bd8cb08ec39642a47
cloudelin <maxcloude@gmail.com>
2014-09-01 11:16:01 +0800

    refactory image view utility.

--- grails-app/assets/javascripts/jquery.imageview.js
+++ grails-app/assets/javascripts/jquery.imageview.js
@@ -1,18 +1,25 @@
+/**
+ * @author Cloude
+ */
 
-$.fn.imageview = function(options) {
-	var array_imageUrl = [];
-	collectAllImageUrl(array_imageUrl, this, options);
-	console.log(array_imageUrl);
-}
-
-function collectAllImageUrl(array, obj, options) {
+(function($) {
+	
+	var settings;
+	var array = [];
+	var arrayLength;
+	
+	$.fn.imageview = function(options) {
+		console.log(options);
+		settings = options;
+		collectAllImageUrl(this);
+		arrayLength = array.length;
+		console.log(array + ', length=' + arrayLength);
+	}
 	
 	var clickDomHandler = function(e) {
-		
 		var ele = $(e.target);
-		var currUrl = ele.attr(options.attrOfUrl);
+		var currUrl = ele.attr(settings.attrOfUrl);
 		var currUrlIdx = array.indexOf(currUrl);
-		var arrayLength = array.length;
 		
 		var imageMaskDiv = $('<div class="imageview-mask-div"></div>');
 		var eleContainer = $('<div class="imageview-element-container"></div>');
@@ -37,49 +44,55 @@ function collectAllImageUrl(array, obj, options) {
 		});
 		
 		imgObj.click(function(e){
-			currUrlIdx++;
-			if (currUrlIdx > (arrayLength-1)) {
-				currUrlIdx = 0;
+			if (arrayLength == 1) {
+				imageMaskDiv.remove();
+			} else {
+				currUrlIdx++;
+				if (currUrlIdx > (arrayLength-1)) {
+					currUrlIdx = 0;
+				}
+				var nextUrl = array[currUrlIdx];
+				imgObj.attr('src', nextUrl);
+				
+				adjustImage(imgObj, imgContainer);
 			}
-			var nextUrl = array[currUrlIdx];
-			imgObj.attr('src', nextUrl);
-			
-			adjustImage(imgObj, imgContainer);
 			e.stopPropagation();
 		});
-		
 		e.stopPropagation();
 	};
 	
-	var obj = $(obj);
-	for (var i=0; i<obj.children().length; i++) {
-		var ele = $(obj.children()[i]);
-		var imageUrl = ele.attr(options.attrOfUrl);
+	function collectAllImageUrl(obj) {
+		var obj = $(obj);
+		var imageUrl = obj.attr(settings.attrOfUrl);
 		if (imageUrl != undefined) {
 			array.push(imageUrl);
-			ele.css('cursor', 'pointer');
-			ele.click(clickDomHandler);
+			obj.css('cursor', 'pointer');
+			obj.click(clickDomHandler);
 		}
-		collectAllImageUrl(array, obj.children()[i], options);
+		for (var i=0; i<obj.children().length; i++) {
+			var ele = $(obj.children()[i]);
+			collectAllImageUrl(obj.children()[i]);
+		}
 	}
-}
-
-function adjustImage(imgObj, imgContainer) {
-	$('<img/>')
-		.attr("src", $(imgObj).attr("src"))
-	    .load(function() {
-	    	if (this.height > this.width) {
-	    		imgContainer.height($(window).height());
-	    		imgObj.css('height', '100%');
-	    		imgObj.css('width', 'auto');
-	    	} else {
-	    		imgContainer.height($(window).height());
-	    		imgObj.css('height', 'auto');
-	    		imgObj.css('max-width', '100%');
-	    		imgObj.css('max-height', '100%');
-	    	}
-	    	$(window).unbind('resize').resize(function(){
-    			imgContainer.height($(window).height());
-    		});
-	    });
-}
+	
+	function adjustImage(imgObj, imgContainer) {
+		$('<img/>')
+			.attr("src", $(imgObj).attr("src"))
+		    .load(function() {
+		    	if (this.height > this.width) {
+		    		imgContainer.height($(window).height());
+		    		imgObj.css('height', '100%');
+		    		imgObj.css('width', 'auto');
+		    	} else {
+		    		imgContainer.height($(window).height());
+		    		imgObj.css('height', 'auto');
+		    		imgObj.css('max-width', '100%');
+		    		imgObj.css('max-height', '100%');
+		    	}
+		    	$(window).unbind('resize').resize(function(){
+	    			imgContainer.height($(window).height());
+	    		});
+		    });
+	}
+	
+}(jQuery));

43e70633455e2443d7ebf7d20f08762372388604
lyhcode <lyhcode@gmail.com>
2014-12-03 18:17:11 +0800

    upgrade asset pipeline plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -87,7 +87,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.0"
+        compile ":asset-pipeline:2.0.9"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.15"

ef1dc0214b65404e72f05c3f06a89e7385fe8b42
cloudelin <maxcloude@gmail.com>
2015-02-06 14:18:03 +0800

    Edited the saved session of host mapping

--- grails-app/conf/kgl/VirtualHostFilters.groovy
+++ grails-app/conf/kgl/VirtualHostFilters.groovy
@@ -1,28 +1,42 @@
 package kgl
 
+import java.awt.GraphicsConfiguration.DefaultBufferCapabilities;
+
+import javax.mail.Session;
+
 class VirtualHostFilters {
 
     def filters = {
         all(controller: '*', action: '*') {
             before = {
-
-                log.info "(VirtualHost) check server hostname ${request.serverName}"
-
-                def vhost = VirtualHost.findByHostname(request.serverName)
-
-                def currentChannel
-
-                if (vhost) {
-                    currentChannel = vhost.channel
-                }
-                else {
-                    currentChannel = Channel.findByIsDefault(true)
-                }
-
-                request.setAttribute('channel', currentChannel)
-                session.channelName = currentChannel.name
-
-                log.info "(VirtualHost) auto mapping to channel[${currentChannel.name}]"
+				
+				if (!session.vhostMapping) {
+					
+					log.info "(VirtualHost) check server hostname ${request.serverName}"
+	
+					def vhost = VirtualHost.findByHostname(request.serverName)
+	
+					def currentChannel
+	
+					if (vhost) {
+						currentChannel = vhost.channel
+					}
+					else {
+						currentChannel = Channel.findByIsDefault(true)
+					}
+	
+					def vhostMapping = [:]
+					vhostMapping.hostName = request.serverName
+					vhostMapping.channelName = currentChannel.name
+					
+					session.vhostMapping = vhostMapping
+					
+//	                request.setAttribute('channel', currentChannel)
+//					session.channelName = currentChannel.name
+	
+					log.info "(VirtualHost) auto mapping to channel[${currentChannel.name}]"
+				}
+                
             }
             after = { Map model ->
 
